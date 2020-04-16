0ec02065710aaf34c7934f363bb98f4bbb79d187
Mauricio Gonzalez <mauglezg21@gmail.com>
2020-04-16 15:20:11 -0500

    made contact with api to retrieve image url

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
-require('dotenv').config();
+// require('dotenv').config();
 
-const api_key = process.env.API_KEY;
+const api_key = "vT69ltJMZ0A3j8U3hAOmZaGjMCL65ASmfBtIPKu4"
+
+const queryURL = 'https://api.nasa.gov/planetary/apod?api_key=vT69ltJMZ0A3j8U3hAOmZaGjMCL65ASmfBtIPKu4'
+
+var requestOptions = {
+    method: 'GET',
+    redirect: 'follow'
+  };
+  
+  fetch("https://api.nasa.gov/planetary/apod?api_key=vT69ltJMZ0A3j8U3hAOmZaGjMCL65ASmfBtIPKu4\n", requestOptions)
+    .then(response => response.json())
+    .then(result => console.log(result.hdurl))
+    .catch(error => console.log('error', error));
